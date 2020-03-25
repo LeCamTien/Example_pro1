1ff6ca168e87583e819458b7eac5b149155b3f2b
LeCamTien <43363505+LeCamTien@users.noreply.github.com>
2020-03-25 20:19:30 +0700

    TC_JOOMLA_ARTICLE_007

--- src/test/java/joomla/testcases/TC_JOOMLA_ARTICLE_007.java
+++ src/test/java/joomla/testcases/TC_JOOMLA_ARTICLE_007.java
@@ -11,7 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TC_JOOMLA_ARTICLE_003 extends TestHelper {
+public class TC_JOOMLA_ARTICLE_007 extends TestHelper {
     public String title = Utilities.randomTitle();
     public String category = "Sample Data-Articles";
     public String content = Utilities.randomContent();
@@ -23,37 +23,42 @@ public class TC_JOOMLA_ARTICLE_003 extends TestHelper {
 
     @BeforeMethod
     public void beforeMethod() {
-        Log.info("------- Pre-conditions -------");
         Log.info("1.Login with valid account");
         loginPage.login(Constant.USERNAME, Constant.PASSWORD);
 
-        Log.info("2.Go to Article page");
+        Log.info("2.Go to Articles page");
         homePage.goToArticlePage();
 
-        Log.info("3.Click on New icon of the top right toolbar");
+        Log.info("3.Click on 'New' icon of the top right toolbar");
         articlesPage.clickButton("New");
 
         Log.info("4.Fill data into all fields");
-        newArticlePage.fillData(title, "Unpublished", category, content);
+        newArticlePage.fillData(title, "Published", category, content);
 
         Log.info("5.Click on 'Save & Close' icon of the top right toolbar");
         newArticlePage.clickButton("Save & Close");
 
-        Log.info("6.Verify the article is saved successfully");
+        Log.info("6.Verify article is saved successfully");
+        Assert.assertTrue(articlesPage.doesArticleDisplay(title), "Created article should display on the article table");
         Assert.assertTrue(articlesPage.doesMessageDisplay("Article saved"), "'Article saved' message should display");
-        Assert.assertTrue(articlesPage.doesArticleDisplay(title), "Created article should display on the articles table");
     }
 
-    @Test(description = "TC_JOOMLA_ARTICLE_003 - Verify that user can publish an unpublish article")
-    public void f() {
+    @Test(description = "TC_JOOMLA_ARTICLE_007 - Verify user can move an article to trash section")
+    public void f() throws InterruptedException {
         Log.info("7.Check on the recently added article's checkbox");
         articlesPage.selectCheckbox(title);
 
-        Log.info("8.Click on 'Publish' icon of the top right toolbar");
-        articlesPage.clickButton("Publish");
+        Log.info("8.Click on 'Trash' icon of the top right toolbar");
+        articlesPage.clickButton("Trash");
 
-        Log.info("9.Verify the article is published successfully");
-        Assert.assertTrue(articlesPage.doesStatusIcondisplay(title, "Publish"), "The icon of the selected item should be showed as 'Publish'");
-        Assert.assertTrue(articlesPage.doesMessageDisplay("article published"), "The '1 article published' message should display");
+        Log.info("9.Verify the confirm message is displayed");
+        Assert.assertTrue(articlesPage.doesMessageDisplay("article trashed"), "'1 article trashes' message should display");
+
+        Log.info("10.Select 'Trashed' item of 'Status' dropdown list");
+        articlesPage.clickBtnSearchTools();
+        articlesPage.selectStatusFilter("Trashed");
+
+        Log.info("11.Verify the deleted article is displayed on the table grid");
+        Assert.assertTrue(articlesPage.doesArticleDisplay(title), "The deleted article should display on the table grid");
     }
 }
